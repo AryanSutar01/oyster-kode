560c9653959368fdea3af17b1796cf9d2b8e4dae
Aryan Sutar <sutararyan1304@gmail.com>
2025-04-21 22:46:21 +0530

    feat: add favicon to website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/logo.png" />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
